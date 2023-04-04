4c3c659af8d7bca8a82f61b996a6b8b2b46128a5
mooring <mooring@live.com>
2023-04-05 00:36:00 +0800

    add license at the beginning of the source code

--- src/getpage.c
+++ src/getpage.c
@@ -1,3 +1,14 @@
+// ******************************************************************
+// Copyright (c) mooring@live.com All rights reserved.
+// This code is licensed under the GNU GENERAL PUBLIC LICENSE Version 3.
+// THE CODE IS PROVIDED ?AS IS?, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
+// THE CODE OR THE USE OR OTHER DEALINGS IN THE CODE.
+// ******************************************************************
 #include "getpage.h"
 
 char proxyConf[PXY_CHRS] = {0};
--- src/getpage.h
+++ src/getpage.h
@@ -1,3 +1,14 @@
+// ******************************************************************
+// Copyright (c) mooring@live.com All rights reserved.
+// This code is licensed under the GNU GENERAL PUBLIC LICENSE Version 3.
+// THE CODE IS PROVIDED ?AS IS?, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
+// THE CODE OR THE USE OR OTHER DEALINGS IN THE CODE.
+// ******************************************************************
 #ifndef GETPAGE_H_INCLUDED
 #define GETPAGE_H_INCLUDED
 
@@ -5,7 +16,6 @@
 #include <string.h>
 #include <stdlib.h>
 
-#pragma once
 #pragma warning(disable: 4996)
 
 #if defined(_WIN32) || defined(_WIN64)
